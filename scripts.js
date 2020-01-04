64f057c2f35b761df8d3860df081668ba8009daf
Kevin <kevinalexanderduran8@gmail.com>
2020-01-03 18:53:50 -0800

    NAVIGATION: fixing bug issue: nav transition wont work for bar class

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var isActive = false;
+
+$('.nav_btn').on('click', function () {
+    if (isActive) {
+        $(this).removeClass('active');
+        $('body').removeClass('menu-open');
+    } else {
+        $(this).addClass('active');
+        $('body').addClass('menu-open');
+    }
+
+    isActive = !isActive;
+});
