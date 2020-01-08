2a870b18e67797a84097ef41ffefe323f339ee45
Kevin <kevinalexanderduran8@gmail.com>
2020-01-08 01:49:50 -0800

    NEW COMMIT OF THE DAY

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 var isActive = false;
 
-$('.nav_button').on('click', function () {
-    if (isActive) {
-        $(this).removeClass('active');
-        $('body').removeClass('menu-open');
-    } else {
-        $(this).addClass('active');
-        $('body').addClass('menu-open');
-    }
+$(".nav_button").on("click", function() {
+  if (isActive) {
+    $(this).removeClass("active forward");
+    $("body").removeClass("menu-open");
+  } else {
+    $(this).addClass("active forward");
+    $("body").addClass("menu-open");
+  }
 
-    isActive = !isActive;
+  isActive = !isActive;
+});
+
+$(".nav_shop").on("click", function() {
+  if (isActive) {
+    $(this).removeClass("active forward");
+    $("body").removeClass("shop-open");
+  } else {
+    $(this).addClass("active forward");
+    $("body").addClass("shop-open");
+  }
+
+  isActive = !isActive;
 });
