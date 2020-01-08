967aacb945708159d4d5bafe9333c46bb56c291e
Kevin <kevinalexanderduran8@gmail.com>
2020-01-08 02:54:43 -0800

    HEADER: header mobile finished

--- scripts.js
+++ scripts.js
@@ -12,14 +12,44 @@ $(".nav_button").on("click", function() {
   isActive = !isActive;
 });
 
-$(".nav_shop").on("click", function() {
+$(".nav_shop, .ex").on("click", function() {
   if (isActive) {
-    $(this).removeClass("active forward");
+    $(this).removeClass("active");
     $("body").removeClass("shop-open");
   } else {
-    $(this).addClass("active forward");
+    $(this).addClass("active");
     $("body").addClass("shop-open");
   }
 
   isActive = !isActive;
 });
+
+
+//SMOOTH SCROLL
+
+window.smoothScroll = function (target) {
+    var scrollContainer = target;
+    do { //find scroll container
+      scrollContainer = scrollContainer.parentNode;
+      if (!scrollContainer) return;
+      scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+  
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+      if (target == scrollContainer) break;
+      targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+  
+    scroll = function (c, a, b, i) {
+      i++;
+      if (i > 30) return;
+      c.scrollTop = a + (b - a) / 30 * i;
+      setTimeout(function () {
+        scroll(c, a, b, i);
+      }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+  }
+  //End of scroll down effect
