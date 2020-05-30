d3b0b612d2d16b97221dd2dac60e03cc2265cde3
Kevin <kevinalexanderduran8@gmail.com>
2020-05-30 03:53:43 -0700

    SHOP: added a small dom display to replace the alert

--- scripts.js
+++ scripts.js
@@ -34,25 +34,27 @@ window.addEventListener('load', (event) => {
 
   //loop through buttons to collect the data of the clicked item
   for (var i = 0; i < yellow_btn_shop.length; i++) {
-    yellow_btn_shop[i].addEventListener("click", function() {
+    yellow_btn_shop[i].addEventListener("click", function () {
       //collect the text of the selected element
       var shop_data = this.parentNode.innerText;
       //create a new element to hold the text , add css class to it
       var shop_data_container = document.createElement('li');
       //place text inside newly created element
       shop_data_container.innerHTML = shop_data;
-      alert(shop_data_container.innerHTML);
+      //instead of alert.insert into HTML
+      var item_add = document.createElement('p');
+      item_add.innerText = 'item added!';
+      var added_container = this.nextSibling;
+      added_container.appendChild(item_add);
+      var added = function () {
+        item_add.remove();
+      }
+      setTimeout(added, 1500);
       //set the location of our selected items
       var shop_list = document.querySelector('.open_menu-items');
       //set selected items in appropriate location
       shop_list.innerHTML += shop_data_container.innerHTML + "<br>"
-  }
+    });
+
+  };
 });
-
-
-
-
-
-//need to possibly click the button
-//that then collects the data from the  item being clicked on
-//somehow transfre that data to a (created?) element that we then append to the menu list?
