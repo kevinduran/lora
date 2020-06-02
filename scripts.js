d3ba5bb9e8baade8de5f48307efb36e92d37f069
Kevin <kevinalexanderduran8@gmail.com>
2020-06-02 06:09:08 -0700

    CHECKPOINT

--- scripts.js
+++ scripts.js
@@ -34,32 +34,45 @@ $(".nav_shop, .ex").on("click", function () {
 
 //SHOPPING CART
 window.addEventListener('load', (event) => {
+  //creating variables
   var yellow_btn_shop = document.getElementsByClassName('yellow_btn');
   var shop_total = document.querySelector('.price_num').innerText;
+
+
+  //loop through all yellow shop buttons...
   for (var i = 0; i < yellow_btn_shop.length; i++) {
+    //than way when you click, it only points to the clicked element
     yellow_btn_shop[i].addEventListener("click", function () {
+      //on click...erase placeholder text
       var shop_list = document.querySelector('.open_menu-items');
       if (shop_list.innerText == 'YOUR CART IS EMPTY') {
-        shop_list.innerHTML = '';
+        shop_list.innerText = '';
       }
+      //BUG this needs fixing. need to remove the previous html
       if (shop_total == '$0.00') {
         shop_total = "";
       }
-      newShopTotal = Number(shop_total);
+
+      var newShopTotal = Number(shop_total);
+      //on click, show yellow ball on cart icon that has a #
       var yellowCircle = document.querySelector('.circle');
       yellowCircle.style.visibility = 'visible';
       yellowCircle.innerHTML++;
+      //use variables to store the heading and price of each item
       var shop_data = this.parentNode.getElementsByClassName('bold')[0].innerHTML;
       var shop_data_two = this.parentNode.getElementsByClassName('bold')[1].innerHTML;
+      //need to store the number in  a variable
       var shop_data_two_number = shop_data_two.slice(-2);
       var newNumber = Number(shop_data_two_number);
+      //BUG need to converte to a stringe after?
       shop_total = newShopTotal + newNumber;
       var shop_total_element = document.createElement('p');
       shop_total_element.innerHTML = shop_total;
-      var yolo = shop_total_element.innerHTML;
+
       var shop_data_container = document.createElement('li');
       shop_data_container.innerHTML += shop_data + " ";
       shop_data_container.innerHTML += shop_data_two;
+      //item added feature
       var item_add = document.createElement('p');
       item_add.classList.add('item_added')
       item_add.innerText = 'item added!';
@@ -69,6 +82,7 @@ window.addEventListener('load', (event) => {
         item_add.remove();
       }
       setTimeout(added, 666);
+      //displaying chosen items to the DOM
       shop_list.innerHTML += shop_data_container.innerHTML + "<br>" + "<br>";
       var replaceChild = document.querySelector('.replace');
       var oldChild = document.querySelector('.price_num');
